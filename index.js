33d0e89e0bec4eab843d32cad3d96b122439483a
veramartin293 <veramartin293@gmail.com>
2021-10-20 19:22:03 -0600

    CORS middleware

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const express = require('express');
 const usersRoutes = require('./routes/users.routes');
 const authRoutes = require('./routes/auth.routes');
 const filesRoutes = require('./routes/files.routes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use('/api/users', usersRoutes);
